90013688580e1a57ad4fcf97fdd68e3030331bc6
Sumit23199702 <sumitkumar23199702@gmail.com>
2024-05-04 17:13:58 +0530

    done

---
+++ JQuery/index.js
@@ -0,0 +1,21 @@
+// $("#btnId1").hide();
+
+// $("#btnId1").click(function () {
+//   $(".para1").hide();
+// });
+
+// $(".btnClass1").click(function () {
+//   $(".para1").show();
+// });
+
+// $(".box").click(function () {
+//   $(this).css("background-color", "blue");
+// });
+
+// $(".box").mouseenter(function () {
+//   $(this).css("background-color", "red");
+// });
+
+// $(".box").mouseleave(function () {
+//   $(this).css("background-color", "green");
+// });
