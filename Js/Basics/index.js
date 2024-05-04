8dd6c2d10594818640af4ae8726c53573318178e
Sumit23199702 <sumitkumar23199702@gmail.com>
2024-05-04 18:08:20 +0530

    done

---
+++ Js/Basics/index.js
@@ -0,0 +1,33 @@
+// function greet() {
+//   alert(`Hello 😁`);
+// }
+
+// ****************************
+// var
+var a = 5;
+var a = 6;
+console.log(a);
+
+// let
+
+let b = 10;
+b = 11;
+console.log(b);
+
+// const
+
+const c = 55;
+// c = 65;
+console.log(c);
+
+// Operators
+
+// let age = prompt("Enter Your Age");
+
+// if (age >= 0 && age < 18) {
+//   alert("Not Elligible for Vote");
+// } else if (age >= 18) {
+//   alert("Elligible");
+// } else {
+//   alert("Please Enter a valid age");
+// }
